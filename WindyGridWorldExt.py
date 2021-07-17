ac1705ad1f6b329cdca8b66cded1ba1e00e984c7
alberto-dominguez <alberto.dominguez@comcast.net>
2021-07-17 15:56:02 -0400

    Update WindyGridWorldExt.py
    (1) Added idle action
    (2) Made cosmetic changes (UDRL -> NSEW and wind -> current)

--- WindyGridWorldExt.py
+++ WindyGridWorldExt.py
@@ -6,108 +6,108 @@
 # ALD 13-JUL-2021 Created wind matrix initialization and added stochastic gusts                     #
 # ALD 16-JUL-2021 Moved gust logic from individual episodes to initial matrix setup (per Paulo)     #
 #                 Changed epsilon from a hard-coded constant to a parameter for experimentation     #
+# ALD 17-JUL-2021 Added idle action; made cosmetic changes (UDRL->NSEW and wind->current)           #
 #####################################################################################################
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# world height
+# world height and width (depth will be ignored)
 WORLD_HEIGHT = 10
-
-# world width
 WORLD_WIDTH = 10
 
-# initialize wind strength matrices to 0
-# TODO - Change dynamics wording from wind to current, matching our subject domain
-WIND_X = np.zeros((WORLD_HEIGHT, WORLD_WIDTH), dtype=int)
-WIND_Y = np.zeros((WORLD_HEIGHT, WORLD_WIDTH), dtype=int)
-# Created a completely arbitrary wind system.
-# left edge has wind to the right (+1) and top edge has wind down (+1)
+# initialize current strength matrices to 0
+CURR_X = np.zeros((WORLD_HEIGHT, WORLD_WIDTH), dtype=int)
+CURR_Y = np.zeros((WORLD_HEIGHT, WORLD_WIDTH), dtype=int)
+# Create an arbitrary current field
+# left edge of the world has current to the east and top edge of the world has current to the south
 for k in range(0, WORLD_HEIGHT):
-    WIND_X[k][0] = 1
-for k in range(1, WORLD_HEIGHT):  # start at 1 to avoid placing both WIND_X and WIND_Y in (0,0)
-    WIND_Y[0][k] = 1
-# other cells are set randomly to either the cell to the left or the cell above
+    CURR_X[k][0] = 1
+for k in range(1, WORLD_HEIGHT):  # start at 1 to avoid placing both CURR_X and CURR_Y at (0,0)
+    CURR_Y[0][k] = 1
+# all other cells are set randomly to either the cell to the immediate west or the cell to the immediate north
 for ii in range(1, WORLD_HEIGHT):
     for jj in range(1, WORLD_WIDTH):
         if np.random.binomial(1, 0.5) == 1:
-            WIND_X[ii][jj] = WIND_X[ii][jj-1]
-            WIND_Y[ii][jj] = WIND_Y[ii][jj-1]
+            CURR_X[ii][jj] = CURR_X[ii][jj-1]
+            CURR_Y[ii][jj] = CURR_Y[ii][jj-1]
         else:
-            WIND_X[ii][jj] = WIND_X[ii-1][jj]
-            WIND_Y[ii][jj] = WIND_Y[ii-1][jj]
-# gusts
+            CURR_X[ii][jj] = CURR_X[ii-1][jj]
+            CURR_Y[ii][jj] = CURR_Y[ii-1][jj]
+# introduce stochastic gusts
 for ii in range(0, WORLD_HEIGHT):
     for jj in range(0, WORLD_WIDTH):
         gust = 1
-        if np.random.binomial(1, 0.1) == 1:  # 10% probability of a wind gust
-            gust = 2
-        if np.random.binomial(1, 0.1) == 1:  # 10% probability of wind changing direction
-            gust = gust * -1
-        WIND_X[ii][jj] = WIND_X[ii][jj] * gust
-        WIND_Y[ii][jj] = WIND_Y[ii][jj] * gust
+        if np.random.binomial(1, 0.1) == 1:  # 10% probability of a gust
+            gust *= 2
+        if np.random.binomial(1, 0.1) == 1:  # 10% probability of current changing direction
+            gust *= -1
+        CURR_X[ii][jj] = CURR_X[ii][jj] * gust
+        CURR_Y[ii][jj] = CURR_Y[ii][jj] * gust
 
-# print wind grid
-print('Wind grid:')
+# print current grid
+print('Current grid:')
 for ii in range(0, WORLD_HEIGHT):
     for jj in range(0, WORLD_WIDTH):
-        wind = abs(WIND_X[ii][jj] + WIND_Y[ii][jj])  # Works because x and y are mutually exclusive
-        print(wind, end='')
+        curr = abs(CURR_X[ii][jj] + CURR_Y[ii][jj])  # Works because x and y are mutually exclusive
+        print(curr, end='')
         direction = ""
-        if WIND_X[ii][jj] > 0:
-            direction = "R"
-        elif WIND_X[ii][jj] < 0:
-            direction = "L"
-        elif WIND_Y[ii][jj] > 0:
-            direction = "D"
-        elif WIND_Y[ii][jj] < 0:
-            direction = "U"
+        if CURR_X[ii][jj] > 0:
+            direction = "E"
+        elif CURR_X[ii][jj] < 0:
+            direction = "W"
+        elif CURR_Y[ii][jj] > 0:
+            direction = "S"
+        elif CURR_Y[ii][jj] < 0:
+            direction = "N"
         print(direction, end='')
         print(" ", end='')
     print()
 
 # possible actions
-# TODO - Rename N,S,E,W
-ACTION_UP = 0
-ACTION_DOWN = 1
-ACTION_LEFT = 2
-ACTION_RIGHT = 3
+IDLE = 0
+MOVE_NORTH = 1
+MOVE_SOUTH = 2
+MOVE_WEST = 3
+MOVE_EAST = 4
 
 # Sarsa step size.  Sarsa algorithm is explained on p. 129 of the textbook.
 ALPHA = 0.5
 
 # reward for each step (what this means is we want to minimize the number of steps)
+# if we introduce diagonal steps, we'll need a separate reward = -sqrt(2) for them - ALD
 REWARD = -1.0
 
 # Start and goal positions of the agent
 START = [0, 0]
 GOAL = [5, 9]
-ACTIONS = [ACTION_UP, ACTION_DOWN, ACTION_LEFT, ACTION_RIGHT]
+ACTIONS = [IDLE, MOVE_NORTH, MOVE_SOUTH, MOVE_WEST, MOVE_EAST]
 
 
 # This function defines how the agent moves on the grid.
-# Note that for improved readability I changed the sign convention from the original code. - ALD
 def step(state, action, eps):
 
     i, j = state
 
+    # question - do we want random action tied to the same value as the one in the epsilon-greedy algorithm
     if np.random.binomial(1, eps) == 1:
         action = np.random.choice(ACTIONS)
-    if action == ACTION_UP:
-        return [max(min(i - 1 + WIND_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + WIND_Y[i][j], WORLD_WIDTH - 1), 0)]
-    elif action == ACTION_DOWN:
-        return [max(min(i + 1 + WIND_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + WIND_Y[i][j], WORLD_WIDTH - 1), 0)]
-    elif action == ACTION_LEFT:
-        return [max(min(i + WIND_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j - 1 + WIND_Y[i][j], WORLD_WIDTH - 1), 0)]
-    elif action == ACTION_RIGHT:
-        return [max(min(i + WIND_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + 1 + WIND_Y[i][j], WORLD_WIDTH - 1), 0)]
+    if action == IDLE:
+        return [max(min(i + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+    if action == MOVE_NORTH:
+        return [max(min(i - 1 + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+    elif action == MOVE_SOUTH:
+        return [max(min(i + 1 + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+    elif action == MOVE_WEST:
+        return [max(min(i + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j - 1 + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+    elif action == MOVE_EAST:
+        return [max(min(i + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + 1 + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
     # Per discussion with Paulo, we are ignoring diagonal moves, at least for the moment, possibly always
-    # TODO - add IDLE as an action
     else:
         assert False  # This should never happen since all potential actions are accounted for
 
 
-# play for an episode
+# play for an episode, return amount of time spent in the episode
 def episode(q_value, eps):
 
     # track the total time steps in this episode
@@ -118,11 +118,11 @@ def episode(q_value, eps):
 
     # choose an action based on epsilon-greedy algorithm.
     if np.random.binomial(1, eps) == 1:
+        # eps% of the time, we select an action randomly
         action = np.random.choice(ACTIONS)
     else:
-        # Most of the time (90%), we select an action greedily.
-        # We select the action associated with the maximum q_value found among all q_values stored in values_
-        # 10% of time, we select an action randomly.
+        # (1-eps)% of the time, we select an action greedily
+        # Select the action associated with the maximum q_value found among all q_values stored in values_
         # Algorithmically:
         # for each action_ and value_ stored in values_
         #   if the variable value_ is maximum in the list values_
@@ -139,7 +139,6 @@ def episode(q_value, eps):
             values_ = q_value[next_state[0], next_state[1], :]
             next_action = np.random.choice(
                 [action_ for action_, value_ in enumerate(values_) if value_ == np.max(values_)])
-
         # Sarsa update
         # For more info about Sarsa Algorithm, please refer to p. 129 of the textbook.
         q_value[state[0], state[1], action] = q_value[state[0], state[1], action] + ALPHA * (
@@ -178,20 +177,23 @@ def figure_6_3(eps):
                 optimal_policy[-1].append('G')
                 continue
             bestAction = np.argmax(q_value[i, j, :])
-            if bestAction == ACTION_UP:
-                optimal_policy[-1].append('U')
-            elif bestAction == ACTION_DOWN:
-                optimal_policy[-1].append('D')
-            elif bestAction == ACTION_LEFT:
-                optimal_policy[-1].append('L')
-            elif bestAction == ACTION_RIGHT:
-                optimal_policy[-1].append('R')
+            if bestAction == MOVE_NORTH:
+                optimal_policy[-1].append('N')
+            elif bestAction == MOVE_SOUTH:
+                optimal_policy[-1].append('S')
+            elif bestAction == MOVE_WEST:
+                optimal_policy[-1].append('W')
+            elif bestAction == MOVE_EAST:
+                optimal_policy[-1].append('E')
+            elif bestAction == IDLE:
+                optimal_policy[-1].append('I')
     print('Optimal policy is:')
     for row in optimal_policy:
         print(row)
 
 
 if __name__ == '__main__':
+    # Experiment with various values of epsilon in the epsilon-greedy algorithm
     figure_6_3(0.2)
     figure_6_3(0.1)
     figure_6_3(0.05)
