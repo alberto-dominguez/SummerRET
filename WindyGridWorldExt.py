ec61ac2f9d58e099be4bf48b231824a5ae2c7ced
alberto-dominguez <alberto.dominguez@comcast.net>
2021-07-23 17:24:51 -0400

    Update WindyGridWorldExt.py
    Implemented double gyre velocity field
    
    Result Set 4

--- WindyGridWorldExt.py
+++ WindyGridWorldExt.py
@@ -1,5 +1,5 @@
 #####################################################################################################
-# Extension of Windy Grid World                                                                     #
+# Double Gyre Reinforcement Learning                                                                #
 #                                                                                                   #
 # Commit history:                                                                                   #
 # ALD 12-JUL-2021 First version, assuming heterogeneous wind conditions across each row and column  #
@@ -11,62 +11,23 @@
 # ALD 20-JUL-2021 Added average time step graph, switched axes on graphs for readability            #
 # ALD 21-JUL-2021 Corrected code so avg graph and agg graph are generated from same data series     #
 # ALD 22-JUL-2021 Added NE, NW, SE, SW actions                                                      #
+# ALD 23-JUL-2021 Implemented double gyre velocity field                                            #
 #####################################################################################################
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+# constants
+PI = math.pi
+E = 0.25
+W = PI / 5
+A = 0.1
+ALPHA = 0.5  # Sarsa step size.  Sarsa algorithm is explained on p. 129 of the textbook.
+
 # world height and width (depth will be ignored)
-WORLD_HEIGHT = 15
-WORLD_WIDTH = 15
-
-# initialize current strength matrices to 0
-CURR_X = np.zeros((WORLD_HEIGHT, WORLD_WIDTH), dtype=int)
-CURR_Y = np.zeros((WORLD_HEIGHT, WORLD_WIDTH), dtype=int)
-# Create an arbitrary current field
-# left edge of the world has current to the east and top edge of the world has current to the south
-for k in range(0, WORLD_HEIGHT):
-    CURR_X[k][0] = 1
-for k in range(1, WORLD_HEIGHT):  # start at 1 to avoid placing both CURR_X and CURR_Y at (0,0)
-    CURR_Y[0][k] = 1
-# all other cells are set randomly to either the cell to the immediate west or the cell to the immediate north
-for ii in range(1, WORLD_HEIGHT):
-    for jj in range(1, WORLD_WIDTH):
-        if np.random.binomial(1, 0.5) == 1:
-            CURR_X[ii][jj] = CURR_X[ii][jj-1]
-            CURR_Y[ii][jj] = CURR_Y[ii][jj-1]
-        else:
-            CURR_X[ii][jj] = CURR_X[ii-1][jj]
-            CURR_Y[ii][jj] = CURR_Y[ii-1][jj]
-# introduce stochastic gusts
-for ii in range(0, WORLD_HEIGHT):
-    for jj in range(0, WORLD_WIDTH):
-        gust = 1
-        if np.random.binomial(1, 0.1) == 1:  # 10% probability of a gust
-            gust *= 2
-        if np.random.binomial(1, 0.1) == 1:  # 10% probability of current changing direction
-            gust *= -1
-        CURR_X[ii][jj] = CURR_X[ii][jj] * gust
-        CURR_Y[ii][jj] = CURR_Y[ii][jj] * gust
-
-# print current grid
-print('Current grid:')
-for ii in range(0, WORLD_HEIGHT):
-    for jj in range(0, WORLD_WIDTH):
-        curr = abs(CURR_X[ii][jj] + CURR_Y[ii][jj])  # Works because x and y are mutually exclusive
-        print(curr, end='')
-        direction = ""
-        if CURR_X[ii][jj] > 0:
-            direction = "E"
-        elif CURR_X[ii][jj] < 0:
-            direction = "W"
-        elif CURR_Y[ii][jj] > 0:
-            direction = "S"
-        elif CURR_Y[ii][jj] < 0:
-            direction = "N"
-        print(direction, end='')
-        print(" ", end='')
-    print()
+WORLD_HEIGHT = 10
+WORLD_WIDTH = 20
 
 # possible actions
 IDLE = 0
@@ -78,14 +39,48 @@ MOVE_NE = 5
 MOVE_NW = 6
 MOVE_SE = 7
 MOVE_SW = 8
-
-# Sarsa step size.  Sarsa algorithm is explained on p. 129 of the textbook.
-ALPHA = 0.5
+ACTIONS = [IDLE, MOVE_NORTH, MOVE_SOUTH, MOVE_EAST, MOVE_WEST, MOVE_NE, MOVE_NW, MOVE_SE, MOVE_SW]
 
 # Start and goal positions of the agent
 START = [0, 0]
-GOAL = [14, 14]
-ACTIONS = [IDLE, MOVE_NORTH, MOVE_SOUTH, MOVE_EAST, MOVE_WEST, MOVE_NE, MOVE_NW, MOVE_SE, MOVE_SW]
+GOAL = [9, 9]
+
+
+def a(t):
+    return E * math.sin(W * t)
+
+
+def b(t):
+    return 1 - 2 * E * math.sin(W * t)
+
+
+def f(xx, t):
+    return a(t) * xx**2 + b(t) * xx
+
+
+def u(xx, yy, t):
+    return -PI * A * math.sin(PI*f(xx, t)) * math.cos(PI * yy)
+
+
+def v(xx, yy, t):
+    return PI * A * math.cos(PI*f(xx, t)) * math.sin(PI * yy) * (2 * a(t) * xx + b(t))
+
+
+# create double gyre velocity field
+# This code is intended to reproduce double gyre stream flow described on the LCS website
+# https://shaddenlab.berkeley.edu/uploads/LCS-tutorial/examples.html#Sec7.1
+# The grid size in this code is 10x20, but the example I am trying to reproduce is 1x2 (hence the /10 in the code)
+CURR_X = np.zeros((WORLD_HEIGHT, WORLD_WIDTH), dtype=float)
+CURR_Y = np.zeros((WORLD_HEIGHT, WORLD_WIDTH), dtype=float)
+for ii in range(0, WORLD_HEIGHT):
+    x = ii / 10
+    for jj in range(0, WORLD_WIDTH):
+        y = jj / 10
+        CURR_X[ii][jj] = v(x, y, 0)
+        CURR_Y[ii][jj] = u(x, y, 0)
+xv, yv = np.meshgrid(np.linspace(0, 2, WORLD_WIDTH), np.linspace(0, 1, WORLD_HEIGHT))
+plt.quiver(xv, yv, CURR_X, CURR_Y)
+plt.show()
 
 
 # This function defines how the agent moves on the grid.
@@ -93,26 +88,28 @@ def step(state, action, gremlin):
     i, j = state
     # gremlin represents the probability that action is random
     # noise/uncertainty to account for unexpected disturbances, modeling error, and/or unknown dynamics
+    dx = int(CURR_X[i][j])
+    dy = int(CURR_Y[i][j])
     if np.random.binomial(1, gremlin) == 1:
         action = np.random.choice(ACTIONS)
     if action == MOVE_NORTH:
-        return [max(min(i - 1 + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i - 1 + dx, WORLD_HEIGHT - 1), 0), max(min(j + dy, WORLD_WIDTH - 1), 0)]
     elif action == MOVE_SOUTH:
-        return [max(min(i + 1 + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i + 1 + dx, WORLD_HEIGHT - 1), 0), max(min(j + dy, WORLD_WIDTH - 1), 0)]
     elif action == MOVE_WEST:
-        return [max(min(i + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j - 1 + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i + dx, WORLD_HEIGHT - 1), 0), max(min(j - 1 + dy, WORLD_WIDTH - 1), 0)]
     elif action == MOVE_EAST:
-        return [max(min(i + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + 1 + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i + dx, WORLD_HEIGHT - 1), 0), max(min(j + 1 + dy, WORLD_WIDTH - 1), 0)]
     elif action == MOVE_NE:
-        return [max(min(i - 1 + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + 1 + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i - 1 + dx, WORLD_HEIGHT - 1), 0), max(min(j + 1 + dy, WORLD_WIDTH - 1), 0)]
     elif action == MOVE_NW:
-        return [max(min(i - 1 + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j - 1 + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i - 1 + dx, WORLD_HEIGHT - 1), 0), max(min(j - 1 + dy, WORLD_WIDTH - 1), 0)]
     elif action == MOVE_SE:
-        return [max(min(i + 1 + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + 1 + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i + 1 + dx, WORLD_HEIGHT - 1), 0), max(min(j + 1 + dy, WORLD_WIDTH - 1), 0)]
     elif action == MOVE_SW:
-        return [max(min(i + 1 + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j - 1 + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i + 1 + dx, WORLD_HEIGHT - 1), 0), max(min(j - 1 + dy, WORLD_WIDTH - 1), 0)]
     else:  # action == IDLE
-        return [max(min(i + CURR_X[i][j], WORLD_HEIGHT - 1), 0), max(min(j + CURR_Y[i][j], WORLD_WIDTH - 1), 0)]
+        return [max(min(i + dx, WORLD_HEIGHT - 1), 0), max(min(j + dy, WORLD_WIDTH - 1), 0)]
 
 
 # play for an episode, return amount of time spent in the episode
@@ -220,6 +217,7 @@ def figure_6_3(eps, gremlin):
 
 
 if __name__ == '__main__':
+
     # Experiment with various values of epsilon in the epsilon-greedy algorithm
     figure_6_3(0.2,  0.1)
     figure_6_3(0.1,  0.1)
@@ -233,6 +231,7 @@ if __name__ == '__main__':
     plt.show()
     plt.figure(1).clear()
     plt.figure(2).clear()
+    
     # Experiment with various values of the noise/uncertainty parameter
     figure_6_3(0.1, 0.2)
     figure_6_3(0.1, 0.1)
